f7a54397c02a01288408a72ef74c3fde523841d2
gixLearning <38102077+gixLearning@users.noreply.github.com>
2018-07-03 14:19:57 +0200

    Trying to get migrations working

---
+++ MVC.CMN/Migrations/ApplicationDbContext/Configuration.cs
@@ -0,0 +1,24 @@
+namespace MVC.CMN.Migrations.ApplicationDbContext
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVC.CMN.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\ApplicationDbContext";
+        }
+
+        protected override void Seed(MVC.CMN.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
