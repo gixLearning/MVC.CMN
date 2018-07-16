90ccff8c6862f8fad18b0b8db74ed0b51cabd4fa
gixLearning <38102077+gixLearning@users.noreply.github.com>
2018-07-16 15:54:30 +0200

    Quotes are good

---
+++ MVC.CMN/Scripts/messageboard.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function() {
+    console.log("ready!");
+
+    //Find out how to register 'click'-event as EVENT.CLICK    
+    //$('.quote').on('click', { foo: "Text To Quote" }, onQuote );
+    $('a[data-trigger="quote"]').on('click', { foo: "Text To Quote" }, onQuote );
+
+    function onQuote(event) {
+      alert(event.data.foo);
+    }
+    
+
+});
